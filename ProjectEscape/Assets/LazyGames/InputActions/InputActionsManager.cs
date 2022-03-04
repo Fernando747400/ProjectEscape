5cafb59af3d07585c1be91e5301f283ce9c3cf2e
Diana Ramos/ Dinorekkos <diana_ramos_g@hotmail.com>
2022-03-04 12:49:49 -0600

    Creation Player Controller and InputsUI

---
+++ ProjectEscape/Assets/LazyGames/InputActions/InputActionsManager.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputActionsManager : MonoBehaviour
+{
+
+    [SerializeField] private PlayerController playerController;
+    private PlayerControls playerControls;
+
+
+
+    
+
+    void Awake()
+    {
+
+    }
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

15de5ef740cce400450b054a9c8001aece95d047
Angel Legorreta <legosurio@hotmail.com>
2022-03-10 08:37:02 -0600

    Unity Errors
    No me reconoce las librerías intentare arreglarlo

--- ProjectEscape/Assets/LazyGames/_Code/Interface/Inventory/Item.cs
+++ ProjectEscape/Assets/LazyGames/_Code/Interface/Inventory/Item.cs
@@ -1,9 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Item : MonoBehaviour
 {
+
+    public int numberOfObjects
+    public Text numberOfObjectsText;
+    public int ID;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +20,6 @@ public class Item : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        NumberOfObjectsText
     }
 }

1c6f995356144b74754c8308c8113692037ccdb3
Raymundo Mosqueda <iracoon2014@gmail.com>
2022-05-22 17:19:37 -0500

    Asset Pack/ AudioManager
    assets applied to Initial Scene

---
+++ ProjectEscape/Assets/LazyGames/_Code/Audio/AudioManager.cs
@@ -0,0 +1,25 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+
+    public Sound[] sounds;
+
+    /// Awake is called when the script instance is being loaded.
+    void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ ProjectEscape/Assets/LazyGames/_Code/Audio/Sound.cs
@@ -0,0 +1,11 @@
+using UnityEngine.Audio;
+using UnityEngine;
+
+[System.Serializable]
+public class Sound : MonoBehaviour
+{
+    public AudioClip clip;
+
+    public float volume;
+    public float pitch;
+}

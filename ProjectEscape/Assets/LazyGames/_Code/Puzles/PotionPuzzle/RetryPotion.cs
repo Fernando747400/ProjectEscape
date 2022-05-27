1ac2814f1e3de3c41c80a18f21593e5cfc1ad625
Daniel Navarrete <daniel_navarrete_05@hotmail.com>
2022-05-26 21:18:01 -0500

    PotionPuzzle 100%)?

---
+++ ProjectEscape/Assets/LazyGames/_Code/Puzles/PotionPuzzle/RetryPotion.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RetryPotion : MonoBehaviour
+{
+    public GameObject FinalPotion;
+    public Material ColorOne;
+    public Material ColorTwo;
+
+    public void Retry()
+    {
+        ColorOne.color = Color.black;
+        ColorTwo.color = Color.black;
+        FinalPotion.gameObject.GetComponent<Renderer>().material.color = Color.white;
+    }
+}

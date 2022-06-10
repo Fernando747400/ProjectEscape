275e7c07a330f13a0ec915dbdb7fa414f4023db8
Fernando747 <fernandojcr5@gmail.com>
2022-06-10 11:54:19 -0500

    APK build

--- ProjectEscape/Assets/LazyGames/_Code/GameManager.cs
+++ ProjectEscape/Assets/LazyGames/_Code/GameManager.cs
@@ -13,7 +13,7 @@ public class GameManager : MonoBehaviour
         current = this;
         DontDestroyOnLoad(this.gameObject);
       
-        Application.targetFrameRate = 30;
+        Application.targetFrameRate = 120;
     }
 
     public event Action OpenShipDoor;
